900a2df16cf32484065f0772b52530bb2b7a61eb
HyxiaoGe <18889592303@163.com>
2023-08-12 01:38:53 +0800

    first commit: project initialize

---
+++ comment-service/src/main/java/com/hyxiao/CommentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.hyxiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hyxiao
+ * @date 12/08/2023 00:59
+ * @description
+ */
+@SpringBootApplication
+public class CommentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommentServiceApplication.class, args);
+    }
+}
---
+++ info-service/src/main/java/com/hyxiao/InfoServiceApplication.java
@@ -0,0 +1,16 @@
+package com.hyxiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hyxiao
+ * @date 12/08/2023 01:00
+ * @description
+ */
+@SpringBootApplication
+public class InfoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InfoServiceApplication.class, args);
+    }
+}
