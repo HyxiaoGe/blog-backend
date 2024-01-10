4932b1b11dc727409730d104cc27c96b74f6b1f4
HyxiaoGe <18889592303@163.com>
2024-01-10 18:21:09 +0800

    refactor: remove sout output

--- comment-service/src/main/java/com/hyxiao/config/CommentMessageListener.java
+++ comment-service/src/main/java/com/hyxiao/config/CommentMessageListener.java
@@ -3,11 +3,13 @@ package com.hyxiao.config;
 import com.hyxiao.comment.dto.CommentDTO;
 import com.hyxiao.comment.repo.CommentRepository;
 import com.hyxiao.util.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class CommentMessageListener {
 
     @Autowired
@@ -15,7 +17,7 @@ public class CommentMessageListener {
 
     @RabbitListener(queues = RabbitMQConfig.COMMENT_QUEUE)
     public void handleMessage(String message) {
-        System.out.println("CommentMessageListener: " + message);
+        log.info("CommentMessageListener: " + message);
         CommentDTO commentDTO = JsonUtil.toObject(message, CommentDTO.class);
 
         this.commentRepository.save(CommentDTO.convertTo(commentDTO));
